5691b7616e570db797a0dafedf14755f779d95bd
Sebastien Mondet <seb@mondet.org>
2017-03-15 18:16:03 -0400

    Port to solvuu-build 0.3.0

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -19,7 +19,6 @@ let lib : Project.item =
     ~findlib_deps
     ~dir:"src/lib"
     ~style:(`Pack project_name)
-    ~pkg:project_name
 
 let make_dockerfiles =
   Project.app ("secotrec-make-dockerfiles")
