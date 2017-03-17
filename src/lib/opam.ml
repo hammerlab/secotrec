da07d36409458920b889faa22757b53b81ba0e75
Sebastien Mondet <seb@mondet.org>
2017-03-17 15:42:48 -0400

    Make `Opam.Pin` more verbose

--- src/lib/opam.ml
+++ src/lib/opam.ml
@@ -16,17 +16,20 @@ module Pin = struct
         exec ["opam"; "remote"; "add"; "mothership"; "https://opam.ocaml.org"];
         exec ["opam"; "remote"; "remove"; "default" ];
         seq_succeeds_or ~name:(sprintf "opam-pins-preparation")
+          ~silent:false
           ~clean_up:[fail] [
           exec ["opam"; "update"];
           exec ["opam"; "--version"];
         ];
         seq_succeeds_or ~name:("opam-pins-no-action")
+          ~silent:false
           ~clean_up:[fail]
           (List.map t_list ~f:(fun t ->
                exec ["opam"; "pin"; "--yes"; "add"; "-n"; "-k"; "git";
                      t.package; t.pin]
              ));
         seq_succeeds_or ~name:("opam-pins-upgrade")
+          ~silent:false
           ~clean_up:[fail] [
           exec (
             ["opam"; "reinstall"; "--yes"]
